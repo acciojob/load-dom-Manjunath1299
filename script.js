e822fe4052586114d9a809c693008daac6bd8615
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-20 16:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // Ensure body is empty before writing the string
+  
   document.body.innerHTML = "";
 
-  // Write the string to the body
   document.body.textContent = "DOM load success";
 });
