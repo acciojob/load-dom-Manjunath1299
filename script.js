59752bee30eb8833ceec357a96ad33a54f8ccbd9
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-20 16:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function () {
+        // Get the body element
+        var body = document.body;
+
+        // Check if the body is empty
+        if (body.children.length === 0) {
+            // Create a new paragraph element
+            var paragraph = document.createElement('p');
+
+            // Set the text content of the paragraph
+            paragraph.textContent = 'DOM load success';
+
+            // Append the paragraph to the body
+            body.appendChild(paragraph);
+        }
+    });
